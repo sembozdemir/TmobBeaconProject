67c9d992e42c423a82be51911d3df44ceada7b1e
sembozdemir <sembozdemir@gmail.com>
2015-07-14 16:47:22 +0300

    Database is done. CRUD operations are implemented. This is the log that strings shows creating sql statements:
    07-14 16:42:56.620  19826-19826/com.tmobtech.tmobbeaconproject D/MAPS IS CREATED﹕ CREATE TABLE maps (map_id INTEGER PRIMARY KEY AUTOINCREMENT, map_name TEXT, img_path TEXT);
    07-14 16:42:56.625  19826-19826/com.tmobtech.tmobbeaconproject D/BEACONS IS CREATED﹕ CREATE TABLE beacons(beacon_id INTEGER PRIMARY KEY AUTOINCREMENT, beacon_name TEXT, mac_address TEXT, apsis REAL, ordinat REAL, beacons_map_id INTEGER, FOREIGN KEY(beacons_map_id) REFERENCES maps(map_id) ON DELETE CASCADE);
    07-14 16:42:56.625  19826-19826/com.tmobtech.tmobbeaconproject D/PLACES IS CREATED﹕ CREATE TABLE places(place_id INTEGER PRIMARY KEY AUTOINCREMENT, place_name TEXT, apsis REAL, ordinat REAL, place_map_id INTEGER, FOREIGN KEY(place_map_id) REFERENCES maps(map_id) ON DELETE CASCADE);
    07-14 16:42:56.630  19826-19826/com.tmobtech.tmobbeaconproject D/MEASURE IS CREATED﹕ CREATE TABLE beacon_measure(beacon_id INTEGER, place_id INTEGER, power REAL, PRIMARY KEY(beacon_id, place_id), FOREIGN KEY(beacon_id) REFERENCES beacons(beacon_id)  ON DELETE CASCADE, FOREIGN KEY(place_id) REFERENCES places(place_id) ON DELETE CASCADE);

--- app/src/main/java/com/tmobtech/tmobbeaconproject/data/MyDbHelper.java
+++ app/src/main/java/com/tmobtech/tmobbeaconproject/data/MyDbHelper.java
@@ -2,6 +2,7 @@ package com.tmobtech.tmobbeaconproject.data;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
@@ -18,16 +19,16 @@ public class MyDbHelper extends SQLiteOpenHelper {
     public static final String TABLE_PLACES_NAME = "places";
     public static final String TABLE_BEACON_MEASURE_NAME = "beacon_measure";
 
-    public static final String COLUMN_MAPS_ID = "map_id";
-    public static final String COLUMN_MAPS_NAME = "map_name";
-    public static final String COLUMN_MAPS_IMAGE_PATH = "img_path";
+    public static final String COLUMN_MAP_ID = "map_id";
+    public static final String COLUMN_MAP_NAME = "map_name";
+    public static final String COLUMN_MAP_IMAGE_PATH = "img_path";
 
-    public static final String COLUMN_BEACONS_ID = "beacon_id";
-    public static final String COLUMN_BEACONS_NAME = "beacon_name";
-    public static final String COLUMN_BEACONS_MAC_ADDRESS = "mac_address";
-    public static final String COLUMN_BEACONS_APSIS = "apsis";
-    public static final String COLUMN_BEACONS_ORDINAT = "ordinat";
-    public static final String COLUMN_BEACONS_MAP_ID = "beacons_map_id";
+    public static final String COLUMN_BEACON_ID = "beacon_id";
+    public static final String COLUMN_BEACON_NAME = "beacon_name";
+    public static final String COLUMN_BEACON_MAC_ADDRESS = "mac_address";
+    public static final String COLUMN_BEACON_APSIS = "apsis";
+    public static final String COLUMN_BEACON_ORDINAT = "ordinat";
+    public static final String COLUMN_BEACON_MAP_ID = "beacons_map_id";
 
     public static final String COLUMN_PLACE_ID = "place_id";
     public static final String COLUMN_PLACE_NAME = "place_name";
@@ -46,30 +47,30 @@ public class MyDbHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         final String SQL_CREATE_TABLE_MAPS = "CREATE TABLE " + TABLE_MAPS_NAME + " (" +
-                COLUMN_MAPS_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                COLUMN_MAPS_NAME + " TEXT, " +
-                COLUMN_MAPS_IMAGE_PATH + " TEXT," +
+                COLUMN_MAP_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COLUMN_MAP_NAME + " TEXT, " +
+                COLUMN_MAP_IMAGE_PATH + " TEXT" +
                 ");";
 
         db.execSQL(SQL_CREATE_TABLE_MAPS);
         Log.d("MAPS IS CREATED", SQL_CREATE_TABLE_MAPS);
 
-        final String SQL_CREATE_TABLE_BEACONS = "CREATE TABLE" + TABLE_BEACONS_NAME + " (" +
-                COLUMN_BEACONS_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                COLUMN_BEACONS_NAME + " TEXT, " +
-                COLUMN_BEACONS_MAC_ADDRESS + " TEXT, " +
-                COLUMN_BEACONS_APSIS + " REAL, " +
-                COLUMN_BEACONS_ORDINAT + " REAL, " +
-                COLUMN_BEACONS_MAP_ID + " INTEGER, " +
+        final String SQL_CREATE_TABLE_BEACONS = "CREATE TABLE " + TABLE_BEACONS_NAME + "(" +
+                COLUMN_BEACON_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COLUMN_BEACON_NAME + " TEXT, " +
+                COLUMN_BEACON_MAC_ADDRESS + " TEXT, " +
+                COLUMN_BEACON_APSIS + " REAL, " +
+                COLUMN_BEACON_ORDINAT + " REAL, " +
+                COLUMN_BEACON_MAP_ID + " INTEGER, " +
                 // foreign key beacons_map_id references map_id
-                "FOREIGN KEY(" + COLUMN_BEACONS_MAP_ID + ") " + "REFERENCES " +
-                    TABLE_MAPS_NAME + "(" + COLUMN_MAPS_ID + ")" +
+                "FOREIGN KEY(" + COLUMN_BEACON_MAP_ID + ") " + "REFERENCES " +
+                    TABLE_MAPS_NAME + "(" + COLUMN_MAP_ID + ")" + " ON DELETE CASCADE" +
                 ");";
 
         db.execSQL(SQL_CREATE_TABLE_BEACONS);
         Log.d("BEACONS IS CREATED", SQL_CREATE_TABLE_BEACONS);
 
-        final String SQL_CREATE_TABLE_PLACES = "CREATE TABLE" + TABLE_PLACES_NAME + " (" +
+        final String SQL_CREATE_TABLE_PLACES = "CREATE TABLE " + TABLE_PLACES_NAME + "(" +
                 COLUMN_PLACE_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
                 COLUMN_PLACE_NAME + " TEXT, " +
                 COLUMN_PLACE_APSIS + " REAL, " +
@@ -77,32 +78,35 @@ public class MyDbHelper extends SQLiteOpenHelper {
                 COLUMN_PLACE_MAP_ID + " INTEGER, " +
                 // foreign key beacons_map_id references map_id
                 "FOREIGN KEY(" + COLUMN_PLACE_MAP_ID + ") " + "REFERENCES " +
-                    TABLE_MAPS_NAME + "(" + COLUMN_MAPS_ID + ")" +
+                    TABLE_MAPS_NAME + "(" + COLUMN_MAP_ID + ")" + " ON DELETE CASCADE" +
                 ");";
 
         db.execSQL(SQL_CREATE_TABLE_PLACES);
         Log.d("PLACES IS CREATED", SQL_CREATE_TABLE_PLACES);
 
-        final String SQL_CREATE_TABLE_BEACON_MEASURE = "CREATE TABLE" + TABLE_BEACON_MEASURE_NAME + " (" +
-                COLUMN_BEACON_MEASURE_BEACON_ID + " INTEGER PRIMARY KEY, " +
-                COLUMN_BEACON_MEASURE_PLACE_ID + " INTEGER PRIMARY KEY, " +
+        final String SQL_CREATE_TABLE_BEACON_MEASURE = "CREATE TABLE " + TABLE_BEACON_MEASURE_NAME + "(" +
+                COLUMN_BEACON_MEASURE_BEACON_ID + " INTEGER, " +
+                COLUMN_BEACON_MEASURE_PLACE_ID + " INTEGER, " +
                 COLUMN_BEACON_MEASURE_POWER + " REAL, " +
+                // composite primary key
+                "PRIMARY KEY(" + COLUMN_BEACON_MEASURE_BEACON_ID + ", " + COLUMN_BEACON_MEASURE_PLACE_ID + "), " +
                 // foreign key beacons_id references map_id
                 "FOREIGN KEY(" + COLUMN_BEACON_MEASURE_BEACON_ID + ") " + "REFERENCES " +
-                    TABLE_BEACONS_NAME + "(" + COLUMN_BEACONS_ID + "), " +
+                    TABLE_BEACONS_NAME + "(" + COLUMN_BEACON_ID + ") " + " ON DELETE CASCADE, " +
                 "FOREIGN KEY(" + COLUMN_BEACON_MEASURE_PLACE_ID + ") " + "REFERENCES " +
-                TABLE_PLACES_NAME + "(" + COLUMN_PLACE_ID + ")" +
+                TABLE_PLACES_NAME + "(" + COLUMN_PLACE_ID + ")" + " ON DELETE CASCADE" +
                 ");";
 
         db.execSQL(SQL_CREATE_TABLE_BEACON_MEASURE);
         Log.d("MEASURE IS CREATED", SQL_CREATE_TABLE_BEACON_MEASURE);
-
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_MAPS_NAME);
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_BEACONS_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_PLACES_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_BEACON_MEASURE_NAME);
         onCreate(db);
     }
 
@@ -111,24 +115,38 @@ public class MyDbHelper extends SQLiteOpenHelper {
     public long insertMap(String name, String imgPath) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues values = new ContentValues();
-        values.put(COLUMN_MAPS_NAME, name);
-        values.put(COLUMN_MAPS_IMAGE_PATH, imgPath);
-        long id = db.insert(TABLE_MAPS_NAME, null, values);
-        db.close();
-        return id;
+        values.put(COLUMN_MAP_NAME, name);
+        values.put(COLUMN_MAP_IMAGE_PATH, imgPath);
+        return db.insert(TABLE_MAPS_NAME, null, values);
+    }
+
+    public long deleteMap(long mapId) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        String whereClause = COLUMN_MAP_ID + " = ?";
+        String[] whereArgs = new String[] {
+                "" + mapId
+        };
+        return db.delete(TABLE_MAPS_NAME, whereClause, whereArgs);
     }
 
     public long insertBeacon(String name, String macAddress, float apsis, float ordinat, long mapId) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues values = new ContentValues();
-        values.put(COLUMN_BEACONS_NAME, name);
-        values.put(COLUMN_BEACONS_MAC_ADDRESS, macAddress);
-        values.put(COLUMN_BEACONS_APSIS, apsis);
-        values.put(COLUMN_BEACONS_ORDINAT, ordinat);
-        values.put(COLUMN_BEACONS_MAP_ID, mapId);
-        long id = db.insert(TABLE_BEACONS_NAME, null, values);
-        db.close();
-        return id;
+        values.put(COLUMN_BEACON_NAME, name);
+        values.put(COLUMN_BEACON_MAC_ADDRESS, macAddress);
+        values.put(COLUMN_BEACON_APSIS, apsis);
+        values.put(COLUMN_BEACON_ORDINAT, ordinat);
+        values.put(COLUMN_BEACON_MAP_ID, mapId);
+        return db.insert(TABLE_BEACONS_NAME, null, values);
+    }
+
+    public int deleteBeacon(long beaconId) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        String whereClause = COLUMN_BEACON_ID + " = ?";
+        String[] whereArgs = new String[] {
+                "" + beaconId
+        };
+        return db.delete(TABLE_BEACONS_NAME, whereClause, whereArgs);
     }
 
     public long insertPlace(String name, float apsis, float ordinat, long mapId) {
@@ -138,10 +156,93 @@ public class MyDbHelper extends SQLiteOpenHelper {
         values.put(COLUMN_PLACE_APSIS, apsis);
         values.put(COLUMN_PLACE_ORDINAT, ordinat);
         values.put(COLUMN_PLACE_MAP_ID, mapId);
-        long id = db.insert(TABLE_PLACES_NAME, null, values);
-        db.close();
-        return id;
+        return db.insert(TABLE_PLACES_NAME, null, values);
     }
 
+    public int deletePlace(long placeId) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        String whereClause = COLUMN_PLACE_ID + " = ?";
+        String[] whereArgs = new String[] {
+                "" + placeId
+        };
+        return db.delete(TABLE_PLACES_NAME, whereClause, whereArgs);
+    }
 
+    public long insertBeaconMeasure(long beaconId, long placeId, float power) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_BEACON_MEASURE_BEACON_ID, beaconId);
+        values.put(COLUMN_BEACON_MEASURE_PLACE_ID, placeId);
+        values.put(COLUMN_BEACON_MEASURE_POWER, power);
+        return db.insert(TABLE_BEACON_MEASURE_NAME, null, values);
+    }
+
+    public Cursor getMaps () {
+        SQLiteDatabase db = this.getReadableDatabase();
+        return db.query(TABLE_MAPS_NAME, null, null, null, null, null, null, null);
+    }
+
+    public Cursor getMapFromId(long mapId) {
+        SQLiteDatabase db = getReadableDatabase();
+        String[] columns = new String[] {
+                COLUMN_MAP_NAME,
+                COLUMN_MAP_IMAGE_PATH
+        };
+        String selection = COLUMN_MAP_ID + " = ?";
+        String[] selectionArgs = new String[] {
+                "" + mapId
+        };
+        return db.query(TABLE_MAPS_NAME,
+                columns,
+                selection,
+                selectionArgs,
+                null, null, null, null);
+    }
+
+    public Cursor getBeaconsAtMap(long mapId) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        String selection = COLUMN_BEACON_MAP_ID + "=?";
+        String[] selectionArgs = new String[] {"" + mapId};
+        String[] columns = new String[] {
+                COLUMN_BEACON_ID,
+                COLUMN_BEACON_NAME,
+                COLUMN_BEACON_MAC_ADDRESS,
+                COLUMN_BEACON_APSIS,
+                COLUMN_BEACON_ORDINAT
+        };
+        return db.query(TABLE_BEACONS_NAME,
+                columns,
+                selection,
+                selectionArgs, null, null, null, null);
+    }
+
+    public Cursor getPlacesAtMap(long mapId) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        String selection = COLUMN_PLACE_MAP_ID + "=?";
+        String[] selectionArgs = new String[] {"" + mapId};
+        String[] columns = new String[] {
+                COLUMN_PLACE_ID,
+                COLUMN_PLACE_NAME,
+                COLUMN_PLACE_APSIS,
+                COLUMN_PLACE_ORDINAT
+        };
+        return db.query(TABLE_PLACES_NAME,
+                columns,
+                selection,
+                selectionArgs, null, null, null, null);
+    }
+
+    public Cursor getMeasurePowersForPlace(long placeId) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        String selection = COLUMN_BEACON_MEASURE_PLACE_ID + "=?";
+        String[] selectionArgs = new String[] {"" + placeId};
+        String[] columns = new String[] {
+                COLUMN_BEACON_MEASURE_BEACON_ID,
+                COLUMN_BEACON_MEASURE_POWER
+        };
+        return db.query(TABLE_BEACON_MEASURE_NAME,
+                columns,
+                selection,
+                selectionArgs, null, null, null, null);
+    }
 }
